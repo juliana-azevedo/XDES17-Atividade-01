7111ce1af2ad348182725b1bec0b69e6672f314e
Gabriel Alves <109393905+gabrielhdsalves@users.noreply.github.com>
2026-08-12 15:08:02 -0300

    chore: add default speed and default offset

--- Assets/Scripts/ScrollBackground.cs
+++ Assets/Scripts/ScrollBackground.cs
@@ -2,22 +2,15 @@ using UnityEngine;
 
 public class ScrollBackground : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
     Material mat; // para guardar o material
-    [SerializeField] float speed; // aparece no editor do Unity para poder alterar a velocidade
-    float offsetX; // para guardar a posição atual de Y
+    /*[SerializeField] */ float speed = 0.5f; // aparece no editor do Unity para poder alterar a velocidade
+    float offsetX = 0; // para guardar a posição atual de Y
 
     void Awake()
     {
         // Pega o Material que está dentro do Renderer deste objeto
         mat = GetComponent<SpriteRenderer>().material;
     }
-    
-    // void Start()
-    // {
-    //     
-    // }
 
     // Update is called once per frame
     void Update()
